1b197f94026d6d70278366efccf20fc4097b9105
Volodymyr <volodyaruzhak@gmail.com>
2023-01-05 02:55:35 +0200

    complete styling for select and Select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+import { Select } from './Select';
+
+const options = [
+  { label: 'First', value: 1},
+  { label: 'Second', value: 2},
+  { label: 'Third', value: 3},
+  { label: 'Fourth', value: 4},
+  { label: 'Fifth', value: 5},
+]
+
 function App() {
-  return <h1>HI</h1>
+  const [value, setValue] = useState(options[0])
+  return (
+    <>
+      <Select options={options} value={value}/>
+    </>
+  )
 }
 
 export default App
